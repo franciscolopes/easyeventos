63e7b974237e3f052e616e8e1bd638e0c6ef5acd
Francisco Lopes <franciscolopesdacunha@gmail.com>
2017-09-29 15:59:00 -0300

    Commit 4: configuração do banco de dados

--- src/main/java/com/franciscolopes/easyeventos/domain/Evento.java
+++ src/main/java/com/franciscolopes/easyeventos/domain/Evento.java
@@ -3,10 +3,20 @@ package com.franciscolopes.easyeventos.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Evento implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	private Integer codEvento;
+	
 	private String nome;
 	private Date dataInicio;
 	private Date dataFim;
