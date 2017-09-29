cfa24e65ba05740365b1e90edbe0beb6604ec924
Francisco Lopes <franciscolopesdacunha@gmail.com>
2017-09-29 13:54:39 -0300

    Primeiro commit: sistema configurado e projeto criado

---
+++ src/main/java/com/franciscolopes/easyeventos/EasyeventosApplication.java
@@ -0,0 +1,12 @@
+package com.franciscolopes.easyeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyeventosApplication.class, args);
+	}
+}
